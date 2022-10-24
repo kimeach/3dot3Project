50038fd2f25dddad2da57427ca2a688ebfba8d21
gmKim <gmkim@castingn.com>
2022-10-24 22:41:03 +0900

    UserInfoRepository.java 수정

--- src/main/java/com/proj/Repository/UserInfoRepository.java
+++ src/main/java/com/proj/Repository/UserInfoRepository.java
@@ -1,9 +1,10 @@
 package com.proj.Repository;
 
+import com.proj.Entity.UserInfoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserInfoRepository  extends JpaRepository<UserInfoRepository,String> {
+public interface UserInfoRepository  extends JpaRepository<UserInfoEntity,String> {
 
 }
